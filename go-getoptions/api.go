e55ebc2b54e19649c2b6b5e4dbde7b5b5e8ae46e
David Gamba <davidgamba@gmail.com>
2021-04-11 23:41:08 -0600

    Create CLITree

--- go-getoptions/api.go
+++ go-getoptions/api.go
@@ -22,10 +22,17 @@ type CLIArg struct {
 	Type     argType
 	Name     string
 	Args     []string
-	Children []CLIArg
+	Children []*CLIArg
 }
 
-func parseCLIArgs(args []string) *CLIArg {
+type CLITree struct {
+	Type     argType
+	Name     string
+	Children []*CLITree
+	Parent   *CLITree
+}
+
+func parseCLIArgs(tree *CLITree, args []string) *CLIArg {
 	// Design: This function could return an array or CLIargs as a parse result
 	// or I could do one level up and have a root CLIarg type with the name of
 	// the program.  Having the root level might be helpful with help generation.
@@ -33,31 +40,62 @@ func parseCLIArgs(args []string) *CLIArg {
 	// The current implementation expects os.Args[1:] as an argument so this
 	// can't expect to receive the os.Args[0] as the first argument.
 
+	// CLI arguments are split by spaces by the shell and passed as individual
+	// strings.  In most cases, a cli argument (one string) represents one option
+	// or one argument, however, in the case of bundling mode a single string can
+	// represent multiple options.
+
+	// When parsing the cli args, there is no way to tell apart a command vs just
+	// text input to the program, one argument to this parser needs to be the
+	// command tree.
+
+	// TODO: Question: How is text input before a command handled? Is it allowed?
+
 	root := &CLIArg{
 		Type:     argTypeProgname,
 		Name:     os.Args[0],
 		Args:     args, // Copy of the original args
-		Children: []CLIArg{},
+		Children: []*CLIArg{},
 	}
 	return root
 }
 
 type GetOpt struct {
+	cliTree *CLITree
 }
 
 type ModifyFn func(string)
 
 func New() *GetOpt {
 	gopt := &GetOpt{}
+	gopt.cliTree = &CLITree{
+		Type:     argTypeProgname,
+		Name:     os.Args[0],
+		Children: []*CLITree{},
+	}
 	return gopt
 }
 
 func (gopt *GetOpt) NewCommand(name string, description string) *GetOpt {
 	cmd := &GetOpt{}
+	tree := &CLITree{
+		Type:     argTypeCommand,
+		Name:     name,
+		Children: []*CLITree{},
+		Parent:   gopt.cliTree,
+	}
+	cmd.cliTree = tree
+	gopt.cliTree.Children = append(gopt.cliTree.Children, tree)
 	return cmd
 }
 
 func (gopt *GetOpt) String(name, def string, fns ...ModifyFn) *string {
+	gopt.cliTree.Children = append(gopt.cliTree.Children, &CLITree{
+		Type:     argTypeOption,
+		Name:     name,
+		Children: []*CLITree{},
+		Parent:   gopt.cliTree,
+	})
 	return nil
 }
 
--- go-getoptions/api_test.go
+++ go-getoptions/api_test.go
@@ -2,10 +2,25 @@ package getoptions
 
 import (
 	"bytes"
+	"os"
+	"reflect"
 	"testing"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func setupLogging() *bytes.Buffer {
+	spew.Config = spew.ConfigState{
+		Indent:                  "  ",
+		MaxDepth:                0,
+		DisableMethods:          false,
+		DisablePointerMethods:   false,
+		DisablePointerAddresses: true,
+		DisableCapacities:       true,
+		ContinueOnMethod:        false,
+		SortKeys:                true,
+		SpewKeys:                false,
+	}
 	s := ""
 	buf := bytes.NewBufferString(s)
 	Logger.SetOutput(buf)
@@ -15,5 +30,39 @@ func setupLogging() *bytes.Buffer {
 func TestBuildTree(t *testing.T) {
 	buf := setupLogging()
 	opt := New()
+	opt.String("opt1", "")
+	cmd := opt.NewCommand("cmd", "")
+	cmd.String("opt2", "")
+	tree := opt.cliTree
+	expectedTree := &CLITree{
+		Type:     argTypeProgname,
+		Name:     os.Args[0],
+		Children: []*CLITree{},
+	}
+	expectedTreeCmd := &CLITree{
+		Type:     argTypeCommand,
+		Name:     "cmd",
+		Parent:   expectedTree,
+		Children: []*CLITree{},
+	}
+	expectedTreeCmd.Children = append(expectedTreeCmd.Children, &CLITree{
+		Type:     argTypeOption,
+		Name:     "opt2",
+		Parent:   expectedTreeCmd,
+		Children: []*CLITree{},
+	})
+	expectedTree.Children = append(expectedTree.Children, []*CLITree{
+		{
+			Type:     argTypeOption,
+			Name:     "opt1",
+			Parent:   expectedTree,
+			Children: []*CLITree{},
+		},
+		expectedTreeCmd,
+	}...)
+
+	if !reflect.DeepEqual(expectedTree, tree) {
+		t.Errorf("expected tree: %s\n got: %s\n", spew.Sdump(expectedTree), spew.Sdump(tree))
+	}
 	t.Cleanup(func() { t.Log(buf.String()) })
 }
