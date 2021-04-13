9b8fe569264c505ac17b58965f1215e7dc2ae5a4
David Gamba <davidgamba@gmail.com>
2021-04-12 22:25:58 -0600

    copy is option

--- go-getoptions/api.go
+++ go-getoptions/api.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 var Logger = log.New(ioutil.Discard, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
@@ -105,4 +107,93 @@ func (gopt *GetOpt) String(name, def string, fns ...ModifyFn) *string {
 }
 
 func (gopt *GetOpt) StringVar(p *string, name, def string, fns ...ModifyFn) {
+}
+
+// Mode - Operation mode for short options
+type Mode int
+
+// Operation modes
+const (
+	Normal Mode = iota
+	Bundling
+	SingleDash
+)
+
+// 1: leading dashes
+// 2: option
+// 3: =arg
+var isOptionRegex = regexp.MustCompile(`^(--?)([^=]+)(.*?)$`)
+
+type optionPair struct {
+	Option string
+	// We allow multiple args in case of splitting on comma.
+	Args []string
+}
+
+/*
+isOption - Enhanced version of isOption, this one returns pairs of options and arguments
+At this level we don't agregate results in case we have -- and then other options, basically we can parse one option at a time.
+This makes the caller have to agregate multiple calls to the same option.
+TODO: Here is where we should handle windows /option types.
+*/
+func isOption(s string, mode Mode) ([]optionPair, bool) {
+	// Handle especial cases
+	if s == "--" {
+		return []optionPair{{Option: "--"}}, false
+	} else if s == "-" {
+		return []optionPair{{Option: "-"}}, true
+	}
+
+	match := isOptionRegex.FindStringSubmatch(s)
+	if len(match) > 0 {
+		// check long option
+		if match[1] == "--" {
+			opt := optionPair{}
+			opt.Option = match[2]
+			args := strings.TrimPrefix(match[3], "=")
+			if args != "" {
+				// TODO: Here is where we could split on comma
+				opt.Args = []string{args}
+			}
+			return []optionPair{opt}, true
+		}
+		// check short option
+		switch mode {
+		case Bundling:
+			opts := []optionPair{}
+			for _, option := range strings.Split(match[2], "") {
+				opt := optionPair{}
+				opt.Option = option
+				opts = append(opts, opt)
+			}
+			if len(opts) > 0 {
+				args := strings.TrimPrefix(match[3], "=")
+				if args != "" {
+					opts[len(opts)-1].Args = []string{args}
+				}
+			}
+			return opts, true
+		case SingleDash:
+			opts := []optionPair{}
+			for _, option := range []string{strings.Split(match[2], "")[0]} {
+				opt := optionPair{}
+				opt.Option = option
+				opts = append(opts, opt)
+			}
+			if len(opts) > 0 {
+				args := strings.Join(strings.Split(match[2], "")[1:], "") + match[3]
+				opts[len(opts)-1].Args = []string{args}
+			}
+			return opts, true
+		default:
+			opt := optionPair{}
+			opt.Option = match[2]
+			args := strings.TrimPrefix(match[3], "=")
+			if args != "" {
+				opt.Args = []string{args}
+			}
+			return []optionPair{opt}, true
+		}
+	}
+	return []optionPair{}, false
 }
--- go-getoptions/api_test.go
+++ go-getoptions/api_test.go
@@ -101,6 +101,19 @@ func TestTrees(t *testing.T) {
 				Args:     []string{},
 				Children: []*CLIArg{},
 			}},
+			{"arg", []string{"opt1"}, &CLIArg{
+				Type: argTypeProgname,
+				Name: os.Args[0],
+				Args: []string{"opt1"},
+				Children: []*CLIArg{
+					{
+						Type:     argTypeOption,
+						Name:     "opt1",
+						Args:     []string{},
+						Children: []*CLIArg{},
+					},
+				},
+			}},
 		}
 		for _, test := range tests {
 			t.Run(test.name, func(t *testing.T) {
@@ -113,4 +126,55 @@ func TestTrees(t *testing.T) {
 	})
 
 	t.Cleanup(func() { t.Log(buf.String()) })
+}
+
+func TestIsOption(t *testing.T) {
+	cases := []struct {
+		name    string
+		in      string
+		mode    Mode
+		optPair []optionPair
+		is      bool
+	}{
+		{"lone dash", "-", Normal, []optionPair{{Option: "-"}}, true},
+		{"lone dash", "-", Bundling, []optionPair{{Option: "-"}}, true},
+		{"lone dash", "-", SingleDash, []optionPair{{Option: "-"}}, true},
+
+		// TODO: Lets not return an option here
+		// Lets let the caller identify this.
+		{"double dash", "--", Normal, []optionPair{{Option: "--"}}, false},
+		{"double dash", "--", Bundling, []optionPair{{Option: "--"}}, false},
+		{"double dash", "--", SingleDash, []optionPair{{Option: "--"}}, false},
+
+		{"no option", "opt", Normal, []optionPair{}, false},
+		{"no option", "opt", Bundling, []optionPair{}, false},
+		{"no option", "opt", SingleDash, []optionPair{}, false},
+
+		{"Long option", "--opt", Normal, []optionPair{{Option: "opt"}}, true},
+		{"Long option", "--opt", Bundling, []optionPair{{Option: "opt"}}, true},
+		{"Long option", "--opt", SingleDash, []optionPair{{Option: "opt"}}, true},
+
+		{"Long option with arg", "--opt=arg", Normal, []optionPair{{Option: "opt", Args: []string{"arg"}}}, true},
+		{"Long option with arg", "--opt=arg", Bundling, []optionPair{{Option: "opt", Args: []string{"arg"}}}, true},
+		{"Long option with arg", "--opt=arg", SingleDash, []optionPair{{Option: "opt", Args: []string{"arg"}}}, true},
+
+		{"short option", "-opt", Normal, []optionPair{{Option: "opt"}}, true},
+		{"short option", "-opt", Bundling, []optionPair{{Option: "o"}, {Option: "p"}, {Option: "t"}}, true},
+		{"short option", "-opt", SingleDash, []optionPair{{Option: "o", Args: []string{"pt"}}}, true},
+
+		{"short option with arg", "-opt=arg", Normal, []optionPair{{Option: "opt", Args: []string{"arg"}}}, true},
+		{"short option with arg", "-opt=arg", Bundling, []optionPair{{Option: "o"}, {Option: "p"}, {Option: "t", Args: []string{"arg"}}}, true},
+		{"short option with arg", "-opt=arg", SingleDash, []optionPair{{Option: "o", Args: []string{"pt=arg"}}}, true},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := setupLogging()
+			optPair, is := isOption(tt.in, tt.mode)
+			if !reflect.DeepEqual(optPair, tt.optPair) || is != tt.is {
+				t.Errorf("isOption(%q, %d) == (%q, %v), want (%q, %v)",
+					tt.in, tt.mode, optPair, is, tt.optPair, tt.is)
+			}
+			t.Log(buf.String())
+		})
+	}
 }
